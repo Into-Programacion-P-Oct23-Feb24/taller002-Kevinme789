98e7d03ec0efd5a0f3b6dc5d4aafa325f3f787ca
kevin <karobles5@utpl.edu.ec>
2023-10-19 16:46:53 -0500

    tercer commit, trabajando con Git y GitHubClassroom

--- Proyecto02/src/proyecto02/Proyecto02.java
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -2,20 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto01;
+package proyecto02;
 
 /**
  *
  * @author LAB.ELECT
  */
-public class Proyecto01 {
+public class Proyecto02 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ejemplo de Java\n" + "Manejado desde GitHub\n" + "Versionado a través de Git\n" + "Kevinme789>");
-        // TODO code application logic here
+    System.out.println("Ejemplo dos básico de Java\n" + "Versionado a través de Git\n" + "Manejado desde GitHub\n" + "Además de usar GitHubClassroom");
     }
     
 }
