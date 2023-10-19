4c916f1ef3f8fbf229437a697f1c7483a4d45635
kevin <karobles5@utpl.edu.ec>
2023-10-19 16:33:09 -0500

    segundo commit

--- Proyecto01.java
+++ Proyecto01.java
@@ -14,7 +14,7 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java\n" + "Versionado a través de Git\n" + "Manejado desde GitHub");
+        System.out.println("Ejemplo de Java\n" + "Manejado desde GitHub\n" + "Versionado a trav�s de Git\n" + "Creado por Kevinme789>");
         // TODO code application logic here
     } 
     
